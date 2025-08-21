3d43c9d20568f4f017f447877478d08486bd14a9
Mahmood Alderazi <88010753+m7mood23@users.noreply.github.com>
2025-08-21 22:13:46 +0300

    level 2 pretty easy

---
+++ pointcheck/rectPerimeter.go
@@ -0,0 +1,17 @@
+package piscine 
+
+func RectPerimeter(w, h int) int {
+    //Edge Case 
+    //check if width OR height is negative 
+    if w < 0 || h < 0 {
+
+        return -1
+    }
+    
+    
+    // calculate perimeter : 2 * (width + height )
+    return   2 * (w + h )
+
+
+
+}
