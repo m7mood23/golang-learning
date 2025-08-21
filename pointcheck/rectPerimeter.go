e4964a96a512424e6fdec38a5fc2b92ca80a7206
Mahmood Alderazi <88010753+m7mood23@users.noreply.github.com>
2025-08-21 22:15:31 +0300

    level 2 pretty easy
    formaul for rectperimeter :
    
    2 * (w + h)

--- pointcheck/rectPerimeter.go
+++ pointcheck/rectPerimeter.go
@@ -1,17 +1,14 @@
-package piscine 
+package piscine
 
 func RectPerimeter(w, h int) int {
-    //Edge Case 
-    //check if width OR height is negative 
-    if w < 0 || h < 0 {
+	//Edge Case
+	//check if width OR height is negative
+	if w < 0 || h < 0 {
 
-        return -1
-    }
-    
-    
-    // calculate perimeter : 2 * (width + height )
-    return   2 * (w + h )
+		return -1
+	}
 
-
+	// calculate perimeter : 2 * (width + height )
+	return 2 * (w + h)
 
 }
