d58c84d2fa2800e64557e7195790987518e15fad
Mahmood Alderazi. <88010753+m7mood23@users.noreply.github.com>
2025-08-28 03:48:44 -0700

    finish printrevcomb

---
+++ pointcheck/printrevcomb.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+
+	for i := '9'; i >= '0'; i-- {
+		for j := '8'; j >= '0'; j-- {
+			for k := '7'; k >= '0'; k-- {
+				if i > j && j > k {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					if i != '2' || j != '1' || k != '0' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+
+			}
+
+		}
+	}
+	z01.PrintRune('\n')
+}
