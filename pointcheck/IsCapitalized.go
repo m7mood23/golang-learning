131b47997a9e2610e573f2d11141d295b11657eb
Mahmood Alderazi. <88010753+m7mood23@users.noreply.github.com>
2025-08-29 14:36:32 +0300

    true and false bruh

---
+++ pointcheck/IsCapitalized.go
@@ -0,0 +1,22 @@
+package piscine
+
+func IsCapitalized(s string) bool {
+    if s == "" {
+        return false  // empty string = false
+    }
+    
+    checkfirstletter := true  // start checking first letter
+    
+    for _, char := range s {  // loop through each character
+        if checkfirstletter {  // if we need to check this char
+            if char >= 'a' && char <= 'z' {  // if lowercase
+                return false  // fail immediately
+            }
+            checkfirstletter = false  // stop checking this word
+        } else if char == ' ' {  // if space found
+            checkfirstletter = true  // check next char
+        }
+    }
+    
+    return true  // all words passed
+}
