bfbdee57601ff669539585005e3803a2ca8ebdff
Mahmood Alderazi. <88010753+m7mood23@users.noreply.github.com>
2025-10-16 15:18:08 +0300

    cleanstrr2 agian bruhhhh

---
+++ pointcheck/cleanstr2.go
@@ -0,0 +1,57 @@
+package main 
+
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:] 
+ 
+
+	if len(args) != 1 {   	       
+		z01.PrintRune('\n') 
+        return
+	}
+
+	
+	input := args[0]
+
+	
+	if input == "" {          
+		z01.PrintRune('\n')    
+        return
+	}
+
+
+	slice := []string{}   
+	word := ""           
+
+	
+	for _, char := range input {
+		if char == ' ' {
+			if word != "" {
+				slice = append(slice, word)
+				word = ""
+			}
+		} else {
+			word += string(char)
+		}
+	}
+
+	// append the last word 
+	if word != "" {
+		slice = append(slice, word)
+	}
+
+	
+	for i, w := range slice {
+		for _, char := range  w{
+			z01.PrintRune(char)
+		}
+		if i < len(slice)-1 {
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune('\n')
+}
