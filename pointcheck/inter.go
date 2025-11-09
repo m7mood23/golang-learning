5cdcaec921d167c3fc8b3d90d5c03de6d0f97bb4
Mahmood Alderazi. <88010753+m7mood23@users.noreply.github.com>
2025-11-09 13:42:32 +0300

    Update inter.go

--- pointcheck/inter.go
+++ pointcheck/inter.go
@@ -1,31 +1,29 @@
 package main
-
 import (
 	"os"
 	"github.com/01-edu/z01"
 )
-
 func main() {
 	
     args:= os.Args[1:]
-
+    
     if len(args) != 2{
         return 
     }
 
-	str1 := os.Args[1]
-	str2 := os.Args[2]
-	printed := ""
 
-	for _, char := range str1 {
-		if seen(str2, char) && !seen(printed, char) {
+	s1 := args[0]
+	s2 := args[1]
+	keep := ""
+
+	for _, char := range s1 {
+		if seen(s2, char) && !seen(keep, char) {
 			z01.PrintRune(char)
-			printed += string(char)
+			keep += string(char)
 		}
 	}
 	z01.PrintRune('\n')
 }
-
 //----------func seen ------------------------------
 func seen(s string, c rune) bool {
 	for _, char := range s {
