cbe0d109de1601d746664f79cf1cbaae3cfc5f4c
Mahmood Alderazi. <88010753+m7mood23@users.noreply.github.com>
2025-11-09 14:35:10 +0300

    Update inter.go

--- pointcheck/inter.go
+++ pointcheck/inter.go
@@ -1,9 +1,7 @@
 package main
-
 import ("os"
 	"github.com/01-edu/z01"
 )
-
 func main() {
 	
     args:= os.Args[1:]
@@ -14,12 +12,12 @@ func main() {
 
 	s1 := args[0]
 	s2 := args[1]
-	keep := ""
+	got := ""
 
 	for _, char := range s1 {
-		if seen(s2, char) && !seen(keep, char) {
+		if seen(s2, char) && !seen(got, char) {
 			z01.PrintRune(char)
-			keep += string(char)
+			got += string(char)
 		}
 	}
 	z01.PrintRune('\n')
